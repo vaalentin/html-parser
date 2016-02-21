ce8b4b40520a389d7dcb727ed132bbe18c18e4fb
Vaalentin <vaalentin@protonmail.com>
2016-02-21 10:02:00 +0100

    add input file read

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 int main(int argc, char** argv) {
+  if(argc < 2) {
+    std::cout << "input file missing" << std::endl;
+    exit(1);
+  }
+
+  const char* uri = *(argv + 1);
+
+  std::ifstream file;
+  file.open(uri);
+
+  std::stringstream stream;
+  stream << file.rdbuf();
+  std::string src = stream.str();
+
   return 0;
 }
