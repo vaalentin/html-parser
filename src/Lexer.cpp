99afb2e18361e7d97e14df882343f0028e7b3070
Vaalentin <vaalentin@protonmail.com>
2016-02-24 17:22:00 +0100

    add Lexer

---
+++ src/Lexer.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+#include "Lexer.h"
+
+Lexer::Lexer(const std::string* src) {
+  m_src = src;
+  m_pos = 0;
+  m_char = m_src->at(m_pos);
+  m_row = 1;
+  m_col = 1;
+}
+
+std::vector<Token> Lexer::getToks() {
+  m_toks.clear();
+
+  while(m_char != EOF) {
+    next();
+  }
+
+  m_toks.push_back(Token ("EOF", "", m_row, m_col));
+
+  return m_toks;
+}
+
+void Lexer::consume() {
+  if(match('\n')) {
+    m_row++;
+    m_col = 1;
+  }
+  else {
+    m_col++;
+  }
+
+  unsigned npos = m_pos + 1;
+
+  if(npos == m_src->length()) {
+    m_char = EOF;
+  }
+  else {
+    m_pos = npos;
+    m_char = m_src->at(m_pos);
+  }
+}
+
+bool Lexer::match(char c) {
+  return m_char == c;
+}
+
+bool Lexer::peek(char c) {
+  return m_pos + 1 < m_src->length()
+    && m_src->at(m_pos + 1) == c;
+}
+
+void Lexer::next() {
+  consume();
+}
---
+++ src/Lexer.h
@@ -0,0 +1,30 @@
+#ifndef H_LEXER
+#define H_LEXER
+
+#include <string>
+#include <vector>
+#include "Token.h"
+
+class Lexer {
+  protected:
+    const std::string* m_src;
+    unsigned m_pos;
+    char m_char;
+    std::vector<Token> m_toks;
+    std::string m_buf;
+
+    int m_row;
+    int m_col;
+
+    void consume();
+    bool match(char c);
+    bool peek(char c);
+
+    virtual void next();
+
+  public:
+    Lexer(const std::string* src);
+    std::vector<Token> getToks();
+};
+
+#endif
